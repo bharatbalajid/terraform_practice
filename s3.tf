8a3e09616d7b36b43166b2b3cda5300a4547fcd7
Balaji <bharatbalaji95@gmail.com>
2022-11-24 19:05:49 +0530

    terraform practice

---
+++ s3.tf
@@ -0,0 +1,13 @@
+# to create random pet name
+resource "random_pet" "balaji" {
+  length    = 7
+  
+
+}
+# create a s3 bucket
+resource "aws_s3_bucket" "mybucket" {
+  bucket = random_pet.balaji.id
+# region = "us-east-1"  
+# acl    = "public-read"
+
+}
