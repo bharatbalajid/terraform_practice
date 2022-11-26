54d53d060982025b9427ba98dbb4286cff8710b4
Balaji <bharatbalaji95@gmail.com>
2022-11-26 14:32:13 +0530

    add

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    # AWS Provider
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.41"
+    }
+    # Random Provider
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
 #provider for us-east-1 region
 provider "aws" {
   profile = "default"
