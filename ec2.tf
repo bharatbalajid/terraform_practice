4be5bbffafcf4e9b056c273b1059c6d4f82f1eae
Balaji <bharatbalaji95@gmail.com>
2022-11-26 15:28:52 +0530

    add

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 # Create a EC2 instance
-resource "aws_instance" "ec2" {
-  ami               = "ami-0b0dcb5067f052a63"
-  availability_zone = "us-east-1a"
-  instance_type     = "t2.micro"
-  tags = {
-    "Name" = "myweb"
-  }
+resource "aws_instance" "my-dev-app" {
+  ami             = "ami-0b0dcb5067f052a63"
+  instance_type   = "t2.micro"
+  key_name        = "mytfkey"
+  subnet_id       = aws_subnet.vpc-dev-public-1.id
+  security_groups = [aws_security_group.dev-sg.id]
+  user_data       = file("apache.sh")
 }
--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,70 @@
-# # # Create a vpc in east region
-# resource "aws_vpc" "myvpceast" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     "Name" = "myvpceast"
-#   }
-# }
-# # # Create a vpc in west region
-# resource "aws_vpc" "myvpcwest1" {
-#   cidr_block = "10.0.0.0/16"
-#   provider   = aws.aws-west-1
-#   tags = {
-#     "Name" = "myvpcwest1"
-#   }
-# }
+# Create VPC
+resource "aws_vpc" "vpc-dev" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "vpc-dev"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "vpc-dev-public-1" {
+  vpc_id                  = aws_vpc.vpc-dev.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+# Create Internet Gateway
+resource "aws_internet_gateway" "vpc-dev-igw" {
+  vpc_id = aws_vpc.vpc-dev.id
+
+}
+# Create Route Table
+resource "aws_route_table" "vpc-dev-public-rt" {
+  vpc_id = aws_vpc.vpc-dev.id
+
+}
+# Create Route in Route Table for Internet Access
+resource "aws_route" "vpc-dev-public-rt-route" {
+  route_table_id         = aws_route_table.vpc-dev-public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc-dev-igw.id
+
+
+}
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "vpc-dev-rta" {
+  route_table_id = aws_route_table.vpc-dev-public-rt.id
+  subnet_id      = aws_subnet.vpc-dev-public-1.id
+
+}
+# Create Security Group in the VPC with port 80, 22 as inbound open
+resource "aws_security_group" "dev-sg" {
+  name        = "dev-vpc-default-sg"
+  description = "Dev Vpc Default Security Group"
+  vpc_id      = aws_vpc.vpc-dev.id
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
