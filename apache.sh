3385f855960b8744ba8855d46ddfdc696e5a245d
Bharat Balaji D <108548870+bharatbalajid@users.noreply.github.com>
2022-12-17 21:29:13 +0530

    add

--- apache.sh
+++ apache.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start  
 sudo systemctl enable httpd
-echo "<h1>Welcome to Blue-Dev-App ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+echo "<h1>Welcome to Blue-App ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
